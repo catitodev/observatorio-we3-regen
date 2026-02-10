842303e4e30f74a908821572be49b421c4cf3f58
catitodev <catitodev@gmail.com>
2026-02-09 22:59:11 -0300

    Add React plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // O 'base' deve ser exatamente o nome do seu repositório entre barras
+  plugins: [react()],
   base: '/observatorio-web3-regen/', 
   build: {
     outDir: 'dist',
